8a2f9f044b4c0603797b36d6ddd6982117cafd29
Victor Sato <127440022+victorsato1@users.noreply.github.com>
2023-06-09 21:21:00 +0100

    cont

--- java/main.js
+++ java/main.js
@@ -1,14 +1,18 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
-    grabCursor: true,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-  
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
