0d996804d8fc72f3700b6f811e975f7498969d5d
Victor Sato <127440022+victorsato1@users.noreply.github.com>
2023-06-10 15:36:16 +0100

    sato

--- java/main.js
+++ java/main.js
@@ -16,3 +16,16 @@ inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
 });
+let menuBox = document.getElementById('menuBox')
+let menuIcon = document.getElementById('menuIcon')
+
+function openIcon() {
+    menuBox.classList.toggle("open-menu")
+
+    if (menuBox.classList.contains("open-menu")) {
+        menuIcon.src ="img/close.png"
+    } else {
+        menuIcon.src ="img/menu.png"
+        
+    }
+}
